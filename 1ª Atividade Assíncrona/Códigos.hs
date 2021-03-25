8122a0d7c3940b959f1f78a95953af2a0c51d6c4
dhiogosantos <67306146+dhiogosantos@users.noreply.github.com>
2021-03-25 11:11:49 -0300

    Add files via upload

---
+++ 1ª Atividade Assíncrona/Códigos.hs
@@ -0,0 +1,13 @@
+--EX 2
+dobro x = x*2
+
+--EX 3
+quadruplicar x = (dobro x) + (dobro x)
+
+--EX 4
+hipotenusa x y = sqrt ((x^2) + (y^2))
+
+--EX 5
+distancia (a,b) (c,d) = sqrt (((c-a)^2) + ((d-b)^2))
+
+
